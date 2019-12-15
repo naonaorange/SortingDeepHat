5a0b8252f2f4e078b0c108c5d464cc2ce356152b
naonaorange <nao.lunalovegood@gmail.com>
2019-12-16 07:36:30 +0900

    modify

--- sorting_deep_hat.py
+++ sorting_deep_hat.py
@@ -1,19 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from keras.models import load_model
+from tensorflow.keras import models
 import cv2
 import numpy as np
 
 class sorting_deep_hat:
+    def __init__(self):
+        self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    def estimate(self, input_image_path, model_path, output_image_path, face_rects, house_names):
+
+    def estimate(self, input_image_path, model_path):
         image = cv2.imread(input_image_path)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        faces = face_cascade.detectMultiScale(gray, 1.1, 3)
+        faces = self.face_cascade.detectMultiScale(gray,\
+                                                    scaleFactor= 1.1,\
+                                                    minNeighbors= 3,\
+                                                    minSize=(70, 70))
 
-        model = load_model(model_path)
-        i = 0
+        ret = []
         for (x, y, w, h) in faces:
             face_image = image[y:y+h, x:x+w]
             face_image = cv2.resize(face_image, (100, 100))
@@ -21,25 +25,21 @@ class sorting_deep_hat:
             b,g,r = cv2.split(face_image)
             in_data = cv2.merge([r,g,b])
             in_data = np.array([in_data / 255.])
-    
-            house = np.argmax(model.predict(in_data))
-            if house == 0:
-                house_names.append('Glyffindor')
-                color = (0, 0, 255)
-            elif house == 1:
-                house_names.append('Hufflpuff')
-                color = (0, 255, 255)
-            elif house == 2:
-                house_names.append('Ravenclaw')
-                color = (255, 0, 0)
-            elif house == 3:
-                house_names.append('Slytherin')
-                color = (0, 255, 0)
-    
-            cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-            cv2.putText(image, house_names[i], (x, y), cv2.FONT_HERSHEY_PLAIN, 2, color, 4)
-            cv2.imwrite(output_image_path, image)
-            i += 1
+
+            model = models.load_model(model_path)    
+            result = np.argmax(model.predict(in_data))
+
+            if result == 0:
+                house_name = 'Glyffindor'
+            elif result == 1:
+                house_name = 'Hufflpuff'
+            elif result == 2:
+                house_name = 'Ravenclaw'
+            elif result == 3:
+                house_name = 'Slytherin'
+
+            ret.append([x, y, w, h, house_name])
+        return ret
         
 if __name__ == '__main__':
     model_path = 'models/sorting_deep_hat.h5'
@@ -49,4 +49,4 @@ if __name__ == '__main__':
     houses = []
 
     sdt = sorting_deep_hat()
-    sdt.estimate(input_image_path, model_path, output_image_path, face_rects, houses)
+    sdt.estimate(input_image_path, model_path)
